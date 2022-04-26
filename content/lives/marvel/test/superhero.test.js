670132f75f7fda026b0e6e68859b481ae57db50e
AbeAlvarez <adv.blvd@gmail.com>
2022-04-26 15:58:27 -0500

    test unit

---
+++ content/lives/marvel/test/superhero.test.js
@@ -0,0 +1,8 @@
+
+
+describe("Test suite dummy description", () => {
+    test('Case  1 dummy', () => {
+        const resultOfSomething = 5 + 5
+        expect(resultOfSomething).toBe(10);
+    });
+})
