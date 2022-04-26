da473a933c60d56343ce6ccb93be1a90a655dc84
AbeAlvarez <adv.blvd@gmail.com>
2022-04-26 16:46:04 -0500

    superhero finished

---
+++ content/lives/marvel/app/superhero.js
@@ -0,0 +1,10 @@
+class Superhero { 
+
+    constructor(name, realName, actor){
+        this.name = name
+        this.realName = realName
+        this.actor = actor
+    }
+}
+
+module.exports = Superhero
--- content/lives/marvel/test/superhero.test.js
+++ content/lives/marvel/test/superhero.test.js
@@ -1,8 +1,21 @@
+const superhero = require ("./../app/superhero")
 
+describe("Unite test for superhero", () => {
+    test('Case 1: Get a superhero', () => {
 
+        const aironman= new superhero("Iron Man", "Tony Stark", "Robert Downey Jr.")
+
+        expect(aironman.name).toBe("Iron Man")
+        expect(aironman.realName).toBe("Tony Stark")
+        expect(aironman.actor).toBe("Robert Downey Jr.")
+    });
+})
+
+/**
 describe("Test suite dummy description", () => {
     test('Case  1 dummy', () => {
         const resultOfSomething = 5 + 5
-        expect(resultOfSomething).toBe(10);
+        expect(resultOfSomething).toBe(8);
     });
 })
+*/
